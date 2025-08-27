e7807d1b2a58de16acdfe4a2b42e337cb4e5b79b
LurpDaDerp <lurpdaderp@gmail.com>
2025-08-26 21:52:22 -0700

    new problem

--- planting/planting.java
+++ planting/planting.java
@@ -12,7 +12,30 @@ public class planting {
         BufferedReader f = new BufferedReader(new FileReader("planting.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("planting.out")));
 
-        out.println("");
+        final int N = Integer.parseInt(f.readLine());
+
+        Map<Integer, ArrayList<Integer>> fieldConnections = new HashMap<>();
+
+        
+        for (int i = 0; i < N - 1; i++) {
+            String[] line = f.readLine().split(" ");
+            int field1 = Integer.parseInt(line[0]);
+            int field2 = Integer.parseInt(line[1]);
+
+            fieldConnections.putIfAbsent(field1, new ArrayList<>());
+            fieldConnections.get(field1).add(field2);
+
+            fieldConnections.putIfAbsent(field2, new ArrayList<>());
+            fieldConnections.get(field2).add(field1);
+        }
+
+        int maxConnections = 0;
+
+        for (ArrayList<Integer> connections : fieldConnections.values()) {
+            maxConnections = Math.max(maxConnections, connections.size());
+        }
+
+        out.println(maxConnections+1);
         out.close();
     }
 }
